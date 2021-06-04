d6f9ea1a15e2f7143e2bb3329ef379d354aa162e
s22615 <s22615@pjwstk.edu.pl>
2021-06-04 12:32:50 +0200

    Start of project

---
+++ src/pl/edu/pjatk/Main.java
@@ -0,0 +1,12 @@
+package pl.edu.pjatk;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame window = new JFrame("ProjektPOJ - system do zamawiania pizzy");
+        window.setLayout(null);
+        JLabel nameApl = new JLabel("Zamawanie pizzy!");
+        JLabel nameSec1 = new JLabel("Rozmiar");
+    }
+}
